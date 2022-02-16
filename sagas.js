8823db1998314dbd8a64993ba6c532b69dda1d6f
HongBeen <redbean-2@naver.com>
2022-02-17 04:56:21 +0900

    finish the totorial

---
+++ sagas.js
@@ -0,0 +1,26 @@
+import { put, call, takeEvery, all } from 'redux-saga/effects'
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
+function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+function* incrementAsync() {
+  // yield delay(1000)
+  yield call(delay, 1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// notice how we now only export the rootSaga
+// single entry point to start all Sagas at once
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
